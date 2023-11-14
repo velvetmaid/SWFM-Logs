2450e4de90d97d0a9573b1b752f5675e5f5519a9
camieuxgoofy <tarsisiusadhi@gmail.com>
2023-11-14 15:36:51 +0700

    refactor: update SQL query for vw user GPS loc

--- vwlocation.sql
+++ vwlocation.sql
@@ -193,7 +193,7 @@ CREATE
 OR REPLACE VIEW wfm_schema.vw_user_mobile_absen_location AS
 SELECT
   DISTINCT tx_user_mobile_management.tx_user_mobile_management_id,
-  ranked.absentype,
+  ranked_absen.absentype,
   tx_location_device.currenttime,
   tx_location_device.longitude,
   tx_location_device.latitude,
@@ -450,3 +450,193 @@ GROUP BY
   tx_user_mobile_management.username
 ORDER BY
   tx_location_device.currenttime DESC;
+
+-- NEW
+CREATE
+OR REPLACE VIEW wfm_schema.vw_user_mobile_absen_location_new2 AS
+SELECT
+  tx_user_mobile_management.tx_user_mobile_management_id,
+  ranked.absentype,
+  tx_location_device.currenttime,
+  tx_location_device.longitude,
+  tx_location_device.latitude,
+  tx_location_device.deviceudid,
+  tx_location_device.deviceplatform,
+  ranked.rank_amount,
+  tx_user_mobile_management.username
+FROM
+  wfm_schema.tx_user_mobile_management
+  JOIN (
+    SELECT
+      tx_location_device.deviceudid :: text,
+      tx_location_device.currenttime,
+      tx_location_device.longitude,
+      tx_location_device.latitude,
+      tx_location_device.deviceplatform,
+      ROW_NUMBER() OVER (
+        PARTITION BY tx_location_device.deviceudid :: text
+        ORDER BY
+          tx_location_device.currenttime DESC
+      ) AS row_num
+    FROM
+      wfm_schema.tx_location_device
+  ) ranked ON tx_user_mobile_management.deviceid :: text = ranked.deviceudid :: text
+  AND ranked.row_num = 1
+  JOIN (
+    SELECT
+      tx_absen.userid,
+      tx_absen.absentype,
+      RANK() OVER (
+        PARTITION BY tx_absen.userid
+        ORDER BY
+          tx_absen.absentime DESC
+      ) AS rank_amount
+    FROM
+      wfm_schema.tx_absen
+  ) ranked_absen ON tx_user_mobile_management.tx_user_mobile_management_id = ranked_absen.userid
+WHERE
+  ranked_absen.rank_amount < 2;
+
+CREATE
+OR REPLACE VIEW wfm_schema.vw_user_mobile_absen_location_new2 AS
+SELECT
+  tx_user_mobile_management.tx_user_mobile_management_id,
+  ranked_absen.absentype,
+  ranked_absen.currenttime,
+  ranked_absen.longitude,
+  ranked_absen.latitude,
+  ranked_absen.deviceudid,
+  ranked_absen.deviceplatform,
+  ranked_absen.rank_amount,
+  tx_user_mobile_management.username
+FROM
+  wfm_schema.tx_user_mobile_management
+  JOIN (
+    SELECT
+      tx_location_device.deviceudid :: text,
+      tx_location_device.currenttime,
+      tx_location_device.longitude,
+      tx_location_device.latitude,
+      tx_location_device.deviceplatform,
+      ROW_NUMBER() OVER (
+        PARTITION BY tx_location_device.deviceudid :: text
+        ORDER BY
+          tx_location_device.currenttime DESC
+      ) AS row_num
+    FROM
+      wfm_schema.tx_location_device
+  ) ranked ON tx_user_mobile_management.deviceid :: text = ranked.deviceudid :: text
+  AND ranked.row_num = 1
+  JOIN (
+    SELECT
+      tx_absen.userid,
+      tx_absen.absentype,
+      RANK() OVER (
+        PARTITION BY tx_absen.userid
+        ORDER BY
+          tx_absen.absentime DESC
+      ) AS rank_amount
+    FROM
+      wfm_schema.tx_absen
+  ) ranked_absen ON tx_user_mobile_management.tx_user_mobile_management_id = ranked_absen.userid
+WHERE
+  ranked_absen.rank_amount < 2;
+
+-- 
+-- 
+-- 
+-- 
+-- 
+-- 
+CREATE
+OR REPLACE VIEW wfm_schema.vw_user_mobile_absen_location_new2 AS
+SELECT
+  tx_user_mobile_management.tx_user_mobile_management_id,
+  ranked_absen.absentype,
+  tx_location_device.currenttime,
+  tx_location_device.longitude,
+  tx_location_device.latitude,
+  tx_location_device.deviceudid,
+  tx_location_device.deviceplatform,
+  ranked.rank_amount,
+  tx_user_mobile_management.username
+FROM
+  wfm_schema.tx_user_mobile_management
+  JOIN (
+    SELECT
+      tx_location_device.deviceudid :: text,
+      tx_location_device.currenttime,
+      tx_location_device.longitude,
+      tx_location_device.latitude,
+      tx_location_device.deviceplatform,
+      ROW_NUMBER() OVER (
+        PARTITION BY tx_location_device.deviceudid :: text
+        ORDER BY
+          tx_location_device.currenttime DESC
+      ) AS row_num
+    FROM
+      wfm_schema.tx_location_device
+  ) AS tx_location_device ON tx_user_mobile_management.deviceid :: text = tx_location_device.deviceudid :: text
+  AND tx_location_device.row_num = 1
+  JOIN (
+    SELECT
+      userid,
+      absentype,
+      RANK() OVER (
+        PARTITION BY userid
+        ORDER BY
+          absentime DESC
+      ) AS rank_amount
+    FROM
+      wfm_schema.tx_absen
+  ) AS ranked_absen ON tx_user_mobile_management.tx_user_mobile_management_id = ranked_absen.userid
+WHERE
+  ranked_absen.rank_amount < 2;
+
+-- ======================= --
+-- NEW MOBILE GPS LOCATION --
+-- ======================= --
+CREATE
+OR REPLACE VIEW wfm_schema.vw_user_mobile_absen_location_new2 AS
+SELECT
+  tx_user_mobile_management.tx_user_mobile_management_id,
+  ranked_absen.absentype,
+  tx_location_device.currenttime,
+  tx_location_device.longitude,
+  tx_location_device.latitude,
+  tx_location_device.deviceudid,
+  tx_location_device.deviceplatform,
+  ranked_absen.rank_amount,
+  tx_user_mobile_management.username
+FROM
+  wfm_schema.tx_user_mobile_management
+  JOIN (
+    SELECT
+      tx_location_device_1.deviceudid :: text AS deviceudid,
+      tx_location_device_1.currenttime,
+      tx_location_device_1.longitude,
+      tx_location_device_1.latitude,
+      tx_location_device_1.deviceplatform,
+      row_number() OVER (
+        PARTITION BY (tx_location_device_1.deviceudid :: text)
+        ORDER BY
+          tx_location_device_1.currenttime DESC
+      ) AS row_num
+    FROM
+      wfm_schema.tx_location_device tx_location_device_1
+  ) tx_location_device ON tx_user_mobile_management.deviceid :: text = tx_location_device.deviceudid
+  AND tx_location_device.row_num = 1
+  JOIN (
+    SELECT
+      tx_absen.userid,
+      tx_absen.absentype,
+      rank() OVER (
+        PARTITION BY tx_absen.userid
+        ORDER BY
+          tx_absen.absentime DESC
+      ) AS rank_amount
+    FROM
+      wfm_schema.tx_absen
+  ) ranked_absen ON tx_user_mobile_management.tx_user_mobile_management_id = ranked_absen.userid
+WHERE
+  ranked_absen.rank_amount < 2;
