aa64030d09c0df0677ef9d815096ec857744b8ee
camieuxgoofy <tarsisiusadhi@gmail.com>
2024-04-03 13:47:41 +0700

    get user with rolename

--- getdata.sql
+++ getdata.sql
@@ -20,15 +20,30 @@ SELECT
     b.area_name,
     c.regional_name,
     d.nop_name,
-    e.cluster_name
+    e.cluster_name,
+    string_agg(DISTINCT t.name :: text, ', ' :: text) AS role_name
 FROM
     wfm_schema.tx_user_mobile_management a
     LEFT JOIN wfm_schema.tm_area b ON a.area_id = b.area_id
     LEFT JOIN wfm_schema.tm_regional c ON a.regional_id = c.regional_id
     LEFT JOIN wfm_schema.tm_nop d ON a.nop_id = d.nop_id
     LEFT JOIN wfm_schema.tm_cluster e ON a.cluster_id = e.cluster_id
+    INNER JOIN wfm_schema.mapping_user_mobile_role m ON a.tx_user_mobile_management_id = m.tx_user_mobile_management_id
+    INNER JOIN wfm_schema.tm_user_role t ON m.role_id = t.tm_user_role_id
 WHERE
-    a.area_id = 'Area4';
+    a.is_active = TRUE
+    AND a.is_delete = FALSE
+    AND (
+        t.code = 'MUSERTS'
+        OR t.code = 'MUSERMBP'
+    )
+GROUP BY
+    a.employee_name,
+    a.email,
+    b.area_name,
+    c.regional_name,
+    d.nop_name,
+    e.cluster_name;
 
 SELECT
     a.employee_name,
@@ -44,4 +59,5 @@ FROM
     LEFT JOIN wfm_schema.tm_nop d ON a.nop_id = d.nop_id
     LEFT JOIN wfm_schema.tm_cluster e ON a.cluster_id = e.cluster_id
 WHERE
-    a.area_id = 'Area4';
+    a.is_active = TRUE
+    AND a.is_delete = FALSE;
