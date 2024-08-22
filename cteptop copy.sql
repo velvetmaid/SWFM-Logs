883234f571d99c24eb898a48bf75242000f120b4
camieuxgoofy <tarsisiusadhi@gmail.com>
2024-08-22 15:43:36 +0700

    dump: 22082024

---
+++ cteptop copy.sql
@@ -0,0 +1,110 @@
+WITH availabillity_ticket AS (
+    SELECT
+        tumm.tx_user_mobile_management_id,
+        string_agg(
+            DISTINCT ttto.ticket_no,
+            ', '
+        ) AS ticket_no,
+        CASE
+            WHEN COUNT(ttto.ticket_no) > 0 THEN 'Not Available'
+            ELSE 'Available'
+        END AS availability_status
+    FROM
+        {tx_user_mobile_management} tumm
+        LEFT JOIN {tx_ticket_terr_opr} ttto ON ttto.pic_id = tumm.tx_user_mobile_management_id::varchar
+        AND ttto.status = 'IN PROGRESS'
+        AND ttto.created_at >= (CURRENT_DATE - '1 day'::interval)
+    GROUP BY
+        tumm.tx_user_mobile_management_id
+),
+ranked_absen AS (
+    SELECT
+        tx_absen.userid,
+        tx_absen.absentype,
+        rank() OVER (
+            PARTITION BY tx_absen.userid
+            ORDER BY
+                tx_absen.absentime DESC
+        ) AS rank_amount
+    FROM
+        {tx_absen}
+),
+row_ranked_absen AS (
+    SELECT
+        userid,
+        absentype,
+        rank_amount
+    FROM
+        ranked_absen
+    WHERE
+        rank_amount = 1
+),
+ranked_devices AS (
+    SELECT
+        tld.deviceudid::text,
+        tld.currenttime,
+        tld.longitude,
+        tld.latitude,
+        tld.deviceplatform,
+        row_number() OVER (
+            PARTITION BY tld.deviceudid
+            ORDER BY
+                tld.currenttime DESC
+        ) AS row_num
+    FROM
+        {tx_location_device} tld
+),
+row_ranked_devices AS (
+    SELECT
+        deviceudid,
+        currenttime,
+        longitude,
+        latitude,
+        deviceplatform
+    FROM
+        ranked_devices
+    WHERE
+        row_num = 1
+),
+list_role AS (
+    SELECT
+        mumr.tx_user_mobile_management_id,
+        string_agg(DISTINCT tur.name, ', ') AS role_name
+    FROM
+        {tm_user_role} tur
+        JOIN {mapping_user_mobile_role} mumr ON tur.tm_user_role_id = mumr.role_id
+    GROUP BY
+        mumr.tx_user_mobile_management_id
+)
+SELECT
+    tumm.tx_user_mobile_management_id,
+    tumm.username,
+    tumm.employee_name,
+    tumm.phone_number,
+    ta.area_id,
+    ta.area_name,
+    tr.regional_id,
+    tr.regional_name,
+    tn.nop_id,
+    tn.nop_name,
+    tc.cluster_id,
+    tc.cluster_name,
+    rra.absentype,
+    rrd.currenttime,
+    rrd.longitude,
+    rrd.latitude,
+    rrd.deviceudid,
+    rrd.deviceplatform,
+    lr.role_name,
+    at.ticket_no,
+    at.availability_status
+FROM
+    {tx_user_mobile_management} tumm
+    LEFT JOIN {tm_area} ta ON tumm.area_id = ta.area_id
+    LEFT JOIN {tm_regional} tr ON tumm.regional_id = tr.regional_id
+    LEFT JOIN {tm_nop} tn ON tumm.nop_id = tn.nop_id
+    LEFT JOIN {tm_cluster} tc ON tumm.cluster_id :: bigint = tc.cluster_id
+    INNER JOIN availabillity_ticket at ON tumm.tx_user_mobile_management_id = at.tx_user_mobile_management_id
+    INNER JOIN row_ranked_devices rrd ON tumm.deviceid = rrd.deviceudid
+    INNER JOIN row_ranked_absen rra ON tumm.tx_user_mobile_management_id = rra.userid
+    INNER JOIN list_role lr ON tumm.tx_user_mobile_management_id = lr.tx_user_mobile_management_id;
