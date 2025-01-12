f860a9abf5a17febc70e46d9a722daefae511221
velvetmaid <tarsisiusadhi@gmail.com>
2025-01-12 20:32:22 +0700

    Refactor SQL files: remove obsolete queries

--- sql/set.sql
+++
@@ -1,5 +0,0 @@
-SELECT nextval('ams_schema.part_catalog_part_catalog_id_seq')
-SELECT setval('ams_schema.part_catalog_part_catalog_id_seq', (SELECT MAX(part_catalog_id) FROM ams_schema.part_catalog)+1);
-
-
-
