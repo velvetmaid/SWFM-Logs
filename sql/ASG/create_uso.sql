610f40a5cc76b754891cec8a4712b898728d7803
velvetmaid <tarsisiusadhi@gmail.com>
2025-02-12 16:47:57 +0700

    feat: enhance tx_asset_safe_guard table

--- sql/ASG/create_uso.sql
+++ sql/ASG/create_uso.sql
@@ -1,44 +1,48 @@
 -- wfm_uso_schema.tx_asset_safe_guard definition
--- Drop table
--- DROP TABLE wfm_uso_schema.tx_asset_safe_guard;
 CREATE TABLE
     wfm_uso_schema.tx_asset_safe_guard (
-        tx_asset_safe_guard_id SERIAL PRIMARY KEY,
-        ticket_no varchar(25),
-        site_id VARCHAR(25) REFERENCES wfm_uso_schema.tm_site(site_id),
-        nama_penjaga varchar(255),
-        phone_num varchar(25),
-        type_pengamanan_site_id INT,
-        regular_fee INT,
-        total_fee INT,
-        type_payment varchar(25),
-        bank_name varchar(25),
-        bank_account varchar(25),
-        bank_account_name varchar(25),
-        notes varchar(255),
-        created_by BIGINT,
-        created_at TIMESTAMP WITHOUT TIME ZONE,
+        -- Primary Key
+        tx_asset_safe_guard_id BIGSERIAL PRIMARY KEY,
+        -- Foreign Keys & Identifiers
+        ticket_no VARCHAR(25) NOT NULL,
+        site_id VARCHAR(25) REFERENCES wfm_uso_schema.tm_site (site_id) NOT NULL,
+        area_id VARCHAR(5) NOT NULL,
+        regional_id VARCHAR(5) NOT NULL,
+        nop_id VARCHAR(30) NOT NULL,
+        cluster_id INT NOT NULL,
+        -- Main Information
+        guard_name VARCHAR(255) NOT NULL,
+        phone_number VARCHAR(25) NOT NULL,
+        site_security_type VARCHAR(50) NOT NULL,
+        payment_type VARCHAR(25) NOT NULL,
+        bank_name VARCHAR(25) NOT NULL,
+        bank_account VARCHAR(25) NOT NULL,
+        bank_account_name VARCHAR(25) NOT NULL,
+        -- Financial Information
+        regular_fee INT NOT NULL,
+        total_fee INT NOT NULL,
+        -- Status & Approval
+        notes VARCHAR(255) NOT NULL,
+        status VARCHAR(255) NOT NULL,
+        approve_by INT NOT NULL,
+        approve_name VARCHAR(255) NOT NULL,
+        approve_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        -- Active & Period Info
+        is_exclude BOOLEAN DEFAULT false NOT NULL,
+        month_period INT NOT NULL,
+        year_period INT NOT NULL,
+        -- Audit Information
+        created_by BIGINT NOT NULL,
+        created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
         modified_by BIGINT,
-        modified_at TIMESTAMP WITHOUT TIME ZONE,
-        review varchar(255),
-        is_approve bool,
-        approve_at TIMESTAMP WITHOUT TIME ZONE,
-        approve_by INT,
-        approve_status varchar(255),
-        period_at TIMESTAMP WITHOUT TIME ZONE,
-        is_active bool,
-        month_period INT,
-        year_period INT
+        modified_at TIMESTAMP WITHOUT TIME ZONE
     );
 
 -- wfm_uso_schema.tx_asset_safe_guard_file definition
--- Drop table
--- DROP TABLE wfm_uso_schema.tx_asset_safe_guard_file;
 CREATE TABLE
     wfm_uso_schema.tx_asset_safe_guard_file (
         tx_asset_safe_guard_file_id VARCHAR(50) PRIMARY KEY,
-        tx_asset_safe_guard_id INT REFERENCES wfm_uso_schema.tx_asset_safe_guard(tx_asset_safe_guard_id),
-        file_name varchar(255),
-        file_sftp_id varchar(50),
-        file_category varchar(255)
+        tx_asset_safe_guard_id INT REFERENCES wfm_uso_schema.tx_asset_safe_guard (tx_asset_safe_guard_id) NOT NULL,
+        file_name varchar(255) NOT NULL,
+        file_sftp_id varchar(50) NOT NULL
     );
