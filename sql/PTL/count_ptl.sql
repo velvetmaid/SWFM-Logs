cff454e4b899334c2be45e7c5ba1508df257127c
camieuxgoofy <tarsisiusadhi@gmail.com>
2025-05-30 23:16:45 +0700

    feat: add SQL queries for counting PTL headers and details

--- sql/PTL/count_ptl.sql
+++ sql/PTL/count_ptl.sql
@@ -94,4 +94,203 @@ UNION
 SELECT
     tx_pengisian_token_listrik_header.ticket_ipas_id
 FROM
-    wfm_schema.tx_pengisian_token_listrik_header
+    wfm_schema.tx_pengisian_token_listrik_header;
+
+
+-- 
+SELECT site_id, id_pelanggan FROM wfm_schema.tx_pengisian_token_listrik_header
+where tahun = 2025 and bulan = 5 
+
+
+
+-- Count PTL Header and Detail
+select
+    tptlh.site_id, tptlh.id_pelanggan::text, tptlh.id_pelanggan_name,tptlh.ticket_ipas_id,
+    tptl.billing_id, tptl.denom_prepaid, tptl.no_token 
+from
+    wfm_schema.tx_pengisian_token_listrik_header tptlh
+    inner join wfm_schema.tx_pengisian_token_listrik tptl on tptlh.ticket_no = tptl.ref_ticket_no
+where
+    tptlh.bulan = 5
+    and tptlh.tahun = 2025
+order by tptlh.site_id;
+
+-- Check PTL Detail does not have PTL Header
+SELECT
+    COUNT(*)
+FROM
+    wfm_schema.tx_pengisian_token_listrik_header tptlh
+WHERE
+    tptlh.bulan = 5
+    AND tptlh.tahun = 2025;
+
+select count(*)
+from
+    wfm_schema.tx_pengisian_token_listrik_header tptlh
+    inner join wfm_schema.tx_pengisian_token_listrik tptl on tptlh.ticket_no = tptl.ref_ticket_no
+where
+    tptlh.bulan = 5
+    and tptlh.tahun = 2025;
+  
+    
+    SELECT site_id, id_pelanggan FROM wfm_schema.tx_pengisian_token_listrik_header
+where tahun = 2025 and bulan = 5;
+
+
+
+SELECT
+    h.bulan,
+    h.tahun,
+    h.count_header AS count_ticket,
+    COALESCE(d.count_detail, 0) AS count_token
+FROM (
+    SELECT
+        bulan,
+        tahun,
+        COUNT(*) AS count_header
+    FROM
+        wfm_schema.tx_pengisian_token_listrik_header
+    GROUP BY
+        bulan,
+        tahun
+) h
+LEFT JOIN (
+    SELECT
+        tptlh.bulan,
+        tptlh.tahun,
+        COUNT(*) AS count_detail
+    FROM
+        wfm_schema.tx_pengisian_token_listrik_header tptlh
+    INNER JOIN
+        wfm_schema.tx_pengisian_token_listrik tptl
+        ON tptlh.ticket_no = tptl.ref_ticket_no
+    GROUP BY
+        tptlh.bulan,
+        tptlh.tahun
+) d ON h.bulan = d.bulan AND h.tahun = d.tahun
+where h.bulan <> 0 or h.tahun <> 0
+ORDER BY
+    h.tahun,
+    h.bulan;
+
+-- Count PTL Header and Detail
+select
+    tptlh.site_id, tptlh.id_pelanggan::text, tptlh.id_pelanggan_name,tptlh.ticket_ipas_id,
+    tptl.billing_id, tptl.denom_prepaid, tptl.no_token 
+from
+    wfm_schema.tx_pengisian_token_listrik_header tptlh
+    inner join wfm_schema.tx_pengisian_token_listrik tptl on tptlh.ticket_no = tptl.ref_ticket_no
+where
+    tptlh.bulan = 5
+    and tptlh.tahun = 2025
+order by tptlh.site_id;
+
+-- Check PTL Detail does not have PTL Header
+SELECT
+    COUNT(*)
+FROM
+    wfm_schema.tx_pengisian_token_listrik_header tptlh
+WHERE
+    tptlh.bulan = 5
+    AND tptlh.tahun = 2025;
+
+select count(*)
+from
+    wfm_schema.tx_pengisian_token_listrik_header tptlh
+    inner join wfm_schema.tx_pengisian_token_listrik tptl on tptlh.ticket_no = tptl.ref_ticket_no
+where
+    tptlh.bulan = 5
+    and tptlh.tahun = 2025;
+  
+    
+    SELECT site_id, id_pelanggan FROM wfm_schema.tx_pengisian_token_listrik_header
+where tahun = 2025 and bulan = 5;
+
+
+
+SELECT
+    h.bulan,
+    h.tahun,
+    h.count_header AS count_ticket,
+    COALESCE(d.count_detail, 0) AS count_token
+FROM (
+    SELECT
+        bulan,
+        tahun,
+        COUNT(*) AS count_header
+    FROM
+        wfm_schema.tx_pengisian_token_listrik_header
+    GROUP BY
+        bulan,
+        tahun
+) h
+LEFT JOIN (
+    SELECT
+        tptlh.bulan,
+        tptlh.tahun,
+        COUNT(*) AS count_detail
+    FROM
+        wfm_schema.tx_pengisian_token_listrik_header tptlh
+    INNER JOIN
+        wfm_schema.tx_pengisian_token_listrik tptl
+        ON tptlh.ticket_no = tptl.ref_ticket_no
+    GROUP BY
+        tptlh.bulan,
+        tptlh.tahun
+) d ON h.bulan = d.bulan AND h.tahun = d.tahun
+where h.bulan <> 0 or h.tahun <> 0
+ORDER BY
+    h.tahun,
+    h.bulan;
+    
+   
+   
+   select
+    CASE h.bulan
+        WHEN 1 THEN 'Januari'
+        WHEN 2 THEN 'Februari'
+        WHEN 3 THEN 'Maret'
+        WHEN 4 THEN 'April'
+        WHEN 5 THEN 'Mei'
+        WHEN 6 THEN 'Juni'
+        WHEN 7 THEN 'Juli'
+        WHEN 8 THEN 'Agustus'
+        WHEN 9 THEN 'September'
+        WHEN 10 THEN 'Oktober'
+        WHEN 11 THEN 'November'
+        WHEN 12 THEN 'Desember'
+    end || '(' || h.bulan || ')' as bulan,
+    h.tahun,
+    h.count_header AS count_ticket,
+    COALESCE(d.count_detail, 0) AS count_token
+FROM (
+    SELECT
+        bulan,
+        tahun,
+        COUNT(*) AS count_header
+    FROM
+        wfm_schema.tx_pengisian_token_listrik_header
+    GROUP BY
+        bulan,
+        tahun
+) h
+LEFT JOIN (
+    SELECT
+        tptlh.bulan,
+        tptlh.tahun,
+        COUNT(*) AS count_detail
+    FROM
+        wfm_schema.tx_pengisian_token_listrik_header tptlh
+    INNER JOIN
+        wfm_schema.tx_pengisian_token_listrik tptl
+        ON tptlh.ticket_no = tptl.ref_ticket_no
+    GROUP BY
+        tptlh.bulan,
+        tptlh.tahun
+) d ON h.bulan = d.bulan AND h.tahun = d.tahun
+WHERE h.bulan <> 0 OR h.tahun <> 0
+ORDER BY h.tahun, h.bulan;
+
+
+select * from wfm_schema.tx_pengisian_token_listrik_header tptlh 
+where tahun = 0
